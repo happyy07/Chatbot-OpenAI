d6f42fc744fea57092373ec2beee37823166f2a3
Happy Patel <patelhappy267@gmail.com>
2024-04-15 14:32:50 -0400

    file uploaded part is done

---
+++ chatbot.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+#Upload pdf files
+st.header("My First Chatbot")
+
+with st.sidebar:
+    st.title("Your Documents")
+    #with the help of st.file_uploader, we can upload a file and this file will be stored in file variable
+    file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
+
+#Extract the text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text=""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+        #st.write(text)
+
+#Break it into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\n",
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    st.write(chunks)
+
